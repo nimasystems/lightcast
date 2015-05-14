5941571fc4f85227ab9f9dba89d9a581e031b905
Martin Kovachev <miracle@nimasystems.com>
2015-05-14 16:12:34 +0300

    Bugfix: controllers were not initialized when invoking partials / decorator

--- source/libs/controller/lcWebController.class.php
+++ source/libs/controller/lcWebController.class.php
@@ -229,6 +229,10 @@ abstract class lcWebController extends lcWebBaseController implements iKeyValueP
     {
         $routing = $this->routing;
 
+        if (!$this->getHasInitialized()) {
+            throw new Exception();
+        }
+
         if (!$routing) {
             return $content;
         }
@@ -290,10 +294,20 @@ abstract class lcWebController extends lcWebBaseController implements iKeyValueP
                             }
 
                             $this->prepareControllerInstance($decorating_controller);
-                            $render_response = $this->renderControllerAction(
-                                $decorating_controller,
-                                $action,
-                                $params);
+
+                            $render_response = null;
+
+                            $decorating_controller->initialize();
+
+                            try {
+                                $render_response = $this->renderControllerAction(
+                                    $decorating_controller,
+                                    $action,
+                                    $params);
+                            } catch (Exception $e) {
+                                $decorating_controller->shutdown();
+                                throw $e;
+                            }
 
                             if ($render_response) {
                                 $controller_content = $render_response['content'];
@@ -319,7 +333,7 @@ abstract class lcWebController extends lcWebBaseController implements iKeyValueP
                 }
 
                 // add debugging information
-                if (DO_DEBUG && $this->show_extra_debugging) {
+                if ($this->show_extra_debugging) {
                     $total_render_time = sprintf('%.0f', (microtime(true) - $render_time) * 1000);
 
                     $_url = htmlspecialchars(($this->parent_plugin ? $this->parent_plugin->getPluginName() . ' :: ' : null) . $module . '/' . $action);
@@ -329,7 +343,7 @@ abstract class lcWebController extends lcWebBaseController implements iKeyValueP
                                 "\n\n" . htmlspecialchars($error_trace) : null) . '">' .*/
                         (!$has_error ? $controller_content :
                             '<div style="color:white;background-color:pink;border:1px solid gray;padding:2px;font-size:10px">Decorator error: ' .
-                            $_url . "\n\n" . nl2br(htmlspecialchars($error_message)) . '</div>') .
+                            $_url . "\n\n" . nl2br(htmlspecialchars($error_message)) . "\n\n" . nl2br(htmlspecialchars(($error_trace))) . '</div>') .
                         /*'</div>' .*/
                         '<!-- DecoratingControllerEnd (' . $_url . ') -->';
 
@@ -378,7 +392,7 @@ abstract class lcWebController extends lcWebBaseController implements iKeyValueP
                 }
 
                 // add debugging information
-                if (DO_DEBUG && $this->show_extra_debugging) {
+                if ($this->show_extra_debugging) {
                     $total_render_time = sprintf('%.0f', (microtime(true) - $render_time) * 1000);
 
                     $_url = htmlspecialchars(($this->parent_plugin ? $this->parent_plugin->getPluginName() . ' :: ' : null) . $module . '/' . $action);
