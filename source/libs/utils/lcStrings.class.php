4651fa7807aaff96e2b95c2e55f5529396258e7f
Martin Kovachev <miracle@nimasystems.com>
2016-01-15 10:46:50 +0200

    Changes

--- source/libs/utils/lcStrings.class.php
+++ source/libs/utils/lcStrings.class.php
@@ -1,35 +1,5 @@
 <?php
-/*
- * Lightcast - A PHP MVC Framework
-* Copyright (C) 2005 Nimasystems Ltd
-*
-* This program is NOT free software; you cannot redistribute and/or modify
-* it's sources under any circumstances without the explicit knowledge and
-* agreement of the rightful owner of the software - Nimasystems Ltd.
-*
-* This program is distributed WITHOUT ANY WARRANTY; without even the
-* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-* PURPOSE.  See the LICENSE.txt file for more information.
-*
-* You should have received a copy of LICENSE.txt file along with this
-* program; if not, write to:
-* NIMASYSTEMS LTD
-* Plovdiv, Bulgaria
-* ZIP Code: 4000
-* Address: 95 "Kapitan Raycho" Str.
-* E-Mail: info@nimasystems.com
 
-
-*/
-
-/**
- * File Description
- * @package File Category
- * @subpackage File Subcategory
- * @changed $Id: lcStrings.class.php 1592 2015-05-22 13:28:31Z mkovachev $
- * @author $Author: mkovachev $
- * @version $Revision: 1592 $
- */
 class lcStrings
 {
     const LATIN_CHARS = 'qwertyuiopasdfghjklzxcvbnm';
