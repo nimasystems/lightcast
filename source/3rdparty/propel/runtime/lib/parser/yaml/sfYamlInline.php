d935e77339aa2ad0892a8d64b364ebe67e1c4507
Martin Kovachev <miracle@nimasystems.com>
2019-07-17 17:04:37 +0300

    Deprecation fix

--- source/3rdparty/propel/runtime/lib/parser/yaml/sfYamlInline.php
+++ source/3rdparty/propel/runtime/lib/parser/yaml/sfYamlInline.php
@@ -384,7 +384,9 @@ EOF;
         if (
             (1 == count($keys) && '0' == $keys[0])
             ||
-            (count($keys) > 1 && array_reduce($keys, create_function('$v,$w', 'return (integer) $v + $w;'), 0) == count($keys) * (count($keys) - 1) / 2)) {
+            (count($keys) > 1 && array_reduce($keys, function ($v, $w) use ($keys) {
+                    return (int)$v + (int)$w == count($keys) * (count($keys) - 1) / 2;
+                }))) {
             $output = [];
             foreach ($value as $val) {
                 $output[] = self::dump($val);
